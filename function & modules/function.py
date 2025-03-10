a8f52e279d5b2224d1ea58ed8107cc58e8db7baa
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-03-10 21:33:43 +0530

    gfg

---
+++ function & modules/function.py
@@ -0,0 +1,16 @@
+# function and modules are not same 
+# modules= A file containing multiple function that particuler file known as module
+# kisi file ke under multiple function = modules
+# function= function is a block of code which can use when we want when we Reuse
+
+
+
+# 1 no retun no Argument
+
+def noreturn_noargument():
+    a=20
+    b=30
+    c=a+b
+    print("Addition :",c)
+
+noreturn_noargument()
--- pathern.py
+++ pathern.py
@@ -532,20 +532,31 @@
 
 # ques 23
 
-for row in range(7):
-    for col in range(5):
-        if((row==0 ) or (col==4)):
-            print("*",end=" ")
-        else:
-            print(" ", end=" ")    
-    print() 
+# for row in range(7):
+#     for col in range(5):
+#         if((row==0 ) or (col==4)):
+#             print("*",end=" ")
+#         else:
+#             print(" ", end=" ")    
+#     print() 
 
 # output
 
-# * * * * * 
-#         *
-#         *
-#         *
-#         *
-#         *
-#         *
+#       * * * * * 
+#               *
+#               *
+#               *
+#               *
+#               *
+#               *
+
+
+# Ques 24
+
+for row in range(7):
+    for col in range(5):
+        if( (row==0 or row==3 or row==6) or ( col==0 or col==4)):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")   
+    print()
