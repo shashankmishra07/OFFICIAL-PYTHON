2a58e129970903f7c525f38914badf3f351dc15a
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-03-17 22:39:42 +0530

    efe

--- function & modules/function.py
+++ function & modules/function.py
@@ -272,6 +272,18 @@ def prime1(num):
         print("not a prime")    
 prime1()
 
+
+def prime1(num):
+    num=int(input(" ENter the no "))
+    ch=0
+    for i in range(1,i+1):
+        if(num%i==0):
+            ch+=1
+    if(ch==2):
+        print("prime no ") 
+    else:
+        print("not a prime")    
+prime1()
  
        
 
