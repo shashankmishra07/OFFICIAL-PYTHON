2b85d63b9b9e9018c16eba9e472c9a39da0e8867
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-03-26 20:02:02 +0530

    ddw

--- function & modules/function.py
+++ function & modules/function.py
@@ -248,31 +248,6 @@
 
 
 
-def prime():
-    n=int(input(" enter no "))
-    ch=0
-    for i in range(1,n+1):
-        if(n%i==0):
-            ch+=1
-    if(ch==2):
-        print(" prime no")
-    else:
-        print(" no ")        
-prime()
-
-def prime1(num):
-    num=int(input(" ENter the no "))
-    ch=0
-    for i in range(1,i+1):
-        if(num%i==0):
-            ch+=1
-    if(ch==2):
-        print("prime no ") 
-    else:
-        print("not a prime")
-        def prime1(num):
-    num=int(input(" ENter the no "))
-   )   
 
 
 
