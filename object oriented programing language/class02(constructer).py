69a577428909d673ab6ce3aeb80d737efa2ba107
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-04-01 22:39:46 +0530

    ff

--- object oriented programing language/class02(constructer).py
+++ object oriented programing language/class02(constructer).py
@@ -10,10 +10,29 @@
 
 #-------------------------------------------
 #jab bhi ham class ke frist method me self pass krte h t0 hame class ke all methods me bhi self pass krna hi hoga
+
+# jab constructer me ham self ke alawa parameter pass krte h
+# self ki help se ham kisi fun ke variable ko other funtion me use kr skte h ya 
+#self frm or lnm means parameter(variable) ki value ko bind krke rakhta h or ham in varibale(parameter / value)  ko dusre funtion me bhii use kr skte h
+#    # withput self ham kisi fun ke varibale ko dusre fun me use nahi kr skte
 class myclass:
     def __init__(self,frm , lmn):
         self.frm=frm
         self.lnm=lmn
-        print(" hello word ")
+        print(" this is a constructer ")
+    def myfun(self):
+        print(" user defined function ",self.frm,  self.lnm)
 
-          
+
+obj=myclass(" shashank", "mishra")    #constructer call autometically when we crete object
+obj.myfun()                           # user define fun ko call krna hota h
+
+
+# out=
+# this is a constructer 
+#  user defined function   shashank mishra
+
+
+#***********************************************************
+
+
