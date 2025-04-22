6e315ad2236c892eb640a4212e439510d66a109e
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-04-22 22:41:53 +0530

    ok

--- GENERATOR/ONE.py
+++ GENERATOR/ONE.py
@@ -17,7 +17,7 @@
 #______________________________________________________________________
 
 
-# wap to print evan no in 1 to 10 using generetor function
+# wap to print evan no in 1 to 10 using generetor Function
 def gen():
     for i in range(1,11):
         if i%2==0:
