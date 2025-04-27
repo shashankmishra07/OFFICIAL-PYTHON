a6cf8e29c9ee07e45e497dc1f63690bd37d1bf5b
shashankmishra07 <iamshashankmishra2003@gmail.com>
2025-04-27 23:48:21 +0530

    27

--- GENERATOR/ONE.py
+++ GENERATOR/ONE.py
@@ -1,4 +1,4 @@
-## wap to print  no in 1 to 100 using generetor function
+# wap to print  no in 1 to 100 using generetor function
 
 # def generator():
 #     for i in range(1,101):
@@ -6,12 +6,12 @@
 
 # # g=generator()
 # # for i in g:
-# #     print(i)             #out = 1to 100   
+# #     print(i)             #output = 1to 100   
 
 #   # or
 
 # for i in generator():
-#     print(i)               #out= 1to 100
+#     print(i)               #output= 1to 100
 
 
 #______________________________________________________________________
@@ -23,5 +23,5 @@ def gen():
         if i%2==0:
           yield i
 for i in gen():
-   print(i)      # out = 2,4,6,8,10
+   print(i)      # output = 2,4,6,8,10
     
